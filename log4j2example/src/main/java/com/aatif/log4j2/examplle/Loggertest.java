44b805f67a50c054920d0db1bbdc6c0b71080944
aatif <aatif@aatif-PC>
2021-12-12 18:59:52 +0530

    Commit for Log4j2 Multiple Logger and Appender Example

---
+++ log4j2example/src/main/java/com/aatif/log4j2/examplle/Loggertest.java
@@ -0,0 +1,22 @@
+
+package com.aatif.log4j2.examplle;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Loggertest {
+	public static void main(String[] args) {
+
+		Logger logger = LogManager.getLogger(Loggertest.class);
+		Logger annLogger = LogManager.getLogger("annLogger");
+		Logger cgLogger = LogManager.getLogger("cgLogger");
+
+		annLogger.info("annLogger");
+		cgLogger.info("cgLogger");
+
+		logger.info("Testing");
+		annLogger.info("annLogger");
+		cgLogger.info("cgLogger");
+
+	}
+}
